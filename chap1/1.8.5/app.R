49dcf27b8d4fcc73e05dc0963758699d1d874847
Le-Huynh <lehuynh200194@gmail.com>
2022-03-20 19:05:53 +0900

    #1: exercise 1.8.5

---
+++ chap1/1.8.5/app.R
@@ -0,0 +1,24 @@
+library(shiny)
+library(ggplot2)
+
+datasets <- c("economics", "faithfuld", "seals")
+
+ui <- fluidPage(
+	selectInput("dataset", "Dataset", choices = datasets),
+	verbatimTextOutput("summary"),
+	plotOutput("plot")
+)
+
+server <- function(input, output, session) {
+	dataset <- reactive({
+		get(input$dataset)
+	})
+	output$summary <- renderPrint({
+		summary(dataset())
+	})
+	output$plot <- renderPlot({
+		plot(dataset())
+	}, res = 96)
+}
+
+shinyApp(ui, server)
