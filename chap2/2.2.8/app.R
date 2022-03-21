c8baccae4fa23a1d5e0eb65561991c7406944521
Le-Huynh <lehuynh200194@gmail.com>
2022-03-21 12:22:33 +0900

    #2: exercise 2.2.8.3

--- chap2/2.2.8/app.R
+++ chap2/2.2.8/app.R
@@ -6,7 +6,13 @@ ui <- fluidPage(
 	sliderInput("date", "When should we deliver?",
 		  value = as.Date("2020-09-17"),
 		  min = as.Date("2020-09-16"),
-		  max = as.Date("2020-09-23"))
+		  max = as.Date("2020-09-23")),
+	sliderInput("range", "",
+		  value = 25,
+		  min = 0,
+		  max = 100,
+		  step = 5,
+		  animate = TRUE)
 	
 )
 server <- function(input, output, session) {
