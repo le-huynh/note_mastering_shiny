eca3cd6ce16c59410727dc45debd2ff515e544b7
Le-Huynh <lehuynh200194@gmail.com>
2022-03-21 12:16:26 +0900

    #2: exercise 2.2.8.2

--- chap2/2.2.8/app.R
+++ chap2/2.2.8/app.R
@@ -2,11 +2,18 @@ library(shiny)
 
 ui <- fluidPage(
 	textInput("name", label = NULL, placeholder = "Your name"),
-	textOutput("greeting")
+	textOutput("greeting"),
+	sliderInput("date", "When should we deliver?",
+		  value = as.Date("2020-09-17"),
+		  min = as.Date("2020-09-16"),
+		  max = as.Date("2020-09-23"))
+	
 )
 server <- function(input, output, session) {
+	
 	output$greeting <- renderText({
 		paste0("Hello ", input$name)
 	})
+	
 }
 shinyApp(ui, server)
