fb6aca2e7b1e81a2d9b9672711fb815ac955a754
Le-Huynh <lehuynh200194@gmail.com>
2022-03-21 18:42:49 +0900

    #2: exercise 2.3.5.3

--- chap2/2.3.5/app.R
+++ chap2/2.3.5/app.R
@@ -3,13 +3,17 @@ library(shiny)
 ui <- fluidPage(
 	plotOutput("plot", 
 		 height = "300px",
-		 width = "700px")
+		 width = "700px"),
+	dataTableOutput("table")
 )
 
 server <- function(input, output, session){
 	output$plot <- renderPlot(plot(1:5, main = "A scatterplot of five random numbers"),
 			      res = 96)
-	
+	output$table <- renderDataTable(mtcars,
+				  options = list(pageLength = 5,
+				  	       ordering = FALSE,
+				  	       searching = FALSE))
 }
 
 shinyApp(ui, server)
