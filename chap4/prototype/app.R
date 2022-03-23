af94be2125c3694b81cf71038fb73d31c8f6e810
Le-Huynh <lehuynh200194@gmail.com>
2022-03-23 10:52:35 +0900

    #3: prototype

---
+++ chap4/prototype/app.R
@@ -0,0 +1,65 @@
+library(here)
+library(dplyr)
+library(ggplot2)
+library(forcats)
+library(vroom)
+library(shiny)
+
+if (!exists("injuries")) {
+	injuries <- vroom::vroom(here("chap4/data/injuries.tsv.gz"))
+	products <- vroom::vroom(here("chap4/data/products.tsv"))
+	population <- vroom::vroom(here("chap4/data/population.tsv"))
+}
+
+#<< ui
+prod_codes <- setNames(products$prod_code, products$title)
+
+ui <- fluidPage(
+	fluidRow(
+		column(6,
+		       selectInput("code", "Product", choices = prod_codes)
+		)
+	),
+	fluidRow(
+		column(4, tableOutput("diag")),
+		column(4, tableOutput("body_part")),
+		column(4, tableOutput("location"))
+	),
+	fluidRow(
+		column(12, plotOutput("age_sex"))
+	)
+)
+#>>
+
+#<< server
+server <- function(input, output, session) {
+	selected <- reactive(injuries %>% filter(prod_code == input$code))
+	
+	output$diag <- renderTable(
+		selected() %>% count(diag, wt = weight, sort = TRUE)
+	)
+	output$body_part <- renderTable(
+		selected() %>% count(body_part, wt = weight, sort = TRUE)
+	)
+	output$location <- renderTable(
+		selected() %>% count(location, wt = weight, sort = TRUE)
+	)
+	
+	summary <- reactive({
+		selected() %>%
+			count(age, sex, wt = weight) %>%
+			left_join(population, by = c("age", "sex")) %>%
+			mutate(rate = n / population * 1e4)
+	})
+	
+	output$age_sex <- renderPlot({
+		summary() %>%
+			ggplot(aes(age, n, colour = sex)) +
+			geom_line() +
+			labs(y = "Estimated number of injuries")
+	}, res = 96)
+}
+#>>
+
+shinyApp(ui, server)
+
